3548cef977ebc9c1fc95039fd76958b689cf6e96
selvapalawsdevops <selvakumar.p02@infosys.com>
2017-02-19 18:24:22 +1100

    Add files via upload

---
+++ Delivery Pipeline/packer/demo-app_userdata.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euf -o pipefail
+exec 1> >(logger -s -t $(basename $0)) 2>&1
+
+# Install SaltStack
+yum -y install https://repo.saltstack.com/yum/amazon/salt-amzn-repo-latest-1.ami.noarch.rpm
+yum clean expire-cache; yum -y install salt-minion; chkconfig salt-minion off
